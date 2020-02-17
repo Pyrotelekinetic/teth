3e6b3571f9e62ead8c4d258783adc4bb60792f9a
Pyrotelekinetic <29682759+Pyrotelekinetic@users.noreply.github.com>
2020-02-17 12:55:37 -0700

    new fundamental approach

--- main.hs
+++ main.hs
@@ -1,17 +1,9 @@
 {-# LANGUAGE LambdaCase #-}
 
-import Parser
-
 import System.Environment
 
 main = do
   args <- getArgs
   case args of
-    ["sed"] -> sed
-
-sed :: IO ()
-sed = do
-  input <- getLine
-  case input of
-    _ -> print "placeholder"
+    _ -> putStrLn "placeholder"
 
